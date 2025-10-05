e3118efbeeba1b7a95e9aa19d81c64bb3eda1a72
Ajay Singh <ajay.sing@pledgent.com>
2025-10-05 15:12:42 +0530

    hello

--- src/components/HospitalsSection.tsx
+++ src/components/HospitalsSection.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
-import { Star, MapPin, Users, Clock, Shield, CheckCircle } from 'lucide-react';
+import { Star, MapPin, Users, Clock, Shield, CheckCircle, Link } from 'lucide-react';
 import hospitalIcon from '@/assets/hospital-icon.jpg';
 
 const HospitalsSection = () => {
@@ -160,7 +160,8 @@ const HospitalsSection = () => {
                       View Details
                     </Button> */}
                     <Button size="sm" className="flex-1 bg-accent hover:bg-accent/90">
-                      Get Quote
+                      {/* Get Quote */}
+                     <Link to="/book"> Get Quote</Link>    
                     </Button>
                   </div>
                 </div>
