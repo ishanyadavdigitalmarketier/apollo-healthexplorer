56c763131b48696b742254d957ed275ace5de92e
Ajay Singh <ajay.sing@pledgent.com>
2025-10-14 15:05:39 +0530

    hello

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -320,8 +320,8 @@ const Header = () => {
             <Link to="/treatments" className={mobileNavLinkClass('/treatments')}>Treatments</Link>
             <Link to="/hospitals" className={mobileNavLinkClass('/hospitals')}>Hospitals</Link>
             <Link to="/doctors" className={mobileNavLinkClass('/doctors')}>Doctors</Link>
-            {/* <Link to="/about" className={mobileNavLinkClass('/about')}>About</Link>
-            <Link to="/blog" className={mobileNavLinkClass('/blog')}>Blog</Link> */}
+            <Link to="/about" className={mobileNavLinkClass('/about')}>About</Link>
+            <Link to="/blog" className={mobileNavLinkClass('/blog')}>Blog</Link>
             <Link to="/contact" className={mobileNavLinkClass('/contact')}>Contact</Link>
             <div className="flex gap-3 mt-4 pt-2">
               <Button size="sm" className="bg-accent hover:bg-accent/90 flex-1" asChild>
