d0fa3759bba4f6ca190d5e90b89afe3a529055c8
Ajay Singh <ajay.sing@pledgent.com>
2025-10-14 16:11:41 +0530

    hello

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -317,12 +317,12 @@ const Header = () => {
         {isMobileMenuOpen && (
           <div className="md:hidden mt-4 pb-4 space-y-2 sm:space-y-4 border-t pt-4">
             <Link to="/" className={mobileNavLinkClass('/')}>Home</Link>
-            {/* <Link to="/treatments" className={mobileNavLinkClass('/treatments')}>Treatments</Link> */}
+            <Link to="/treatments" className={mobileNavLinkClass('/treatments')}>Treatments</Link>
             <Link to="/hospitals" className={mobileNavLinkClass('/hospitals')}>Hospitals</Link>
             <Link to="/doctors" className={mobileNavLinkClass('/doctors')}>Doctors</Link>
             <Link to="/about" className={mobileNavLinkClass('/about')}>About</Link>
             <Link to="/blog" className={mobileNavLinkClass('/blog')}>Blog</Link>
-            <Link to="/contact" className={mobileNavLinkClass('/contact')}>Contact</Link>
+            <Link to="/contact" className={mobileNavLinkClass('/contact')}>Contactgggg</Link>
             <div className="flex gap-3 mt-4 pt-2">
               <Button size="sm" className="bg-accent hover:bg-accent/90 flex-1" asChild>
                 <Link to="/book">Book Consultation</Link>
