7a13e553f47b1e96685a8095e740db6e79ba15c2
Ajay Singh <ajay.sing@pledgent.com>
2025-10-14 15:57:45 +0530

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import About from "./pages/about";
 import Doctors from "./pages/doctors";
