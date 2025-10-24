1cc86bf578c237343ff1e2b777e9a6bcc406b48c
Ajay Singh <ajay.sing@pledgent.com>
2025-10-24 15:11:31 +0530

    hello

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -97,7 +97,7 @@ const CTASection = () => {
                   className="w-5 h-5 sm:w-6 sm:h-6 flex-shrink-0" 
                   title="Chat with Us"
                 />
-                <span>{t('whatsappChat')}</span>
+                <span>{t('Chat')}</span>
               </a>
             </Button>
           </div>
